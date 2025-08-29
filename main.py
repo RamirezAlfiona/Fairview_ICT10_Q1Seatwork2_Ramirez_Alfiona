7be171c21f183c744db52f6e99109bef685aad93
RamirezAlfiona <ramirez.afg@obmontessori.edu.ph>
2025-08-29 09:04:47 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyscript import display
+
+restaurant_name = "Reni's Pastries" #string
+owner_name = "Alfiona Ramirez" #string
+year_established = 2025 #integer
+popular_item_price = 180 #integer
+has_delivery = True #boolean
+product_names = ["Chocolate Cake", "Blueberry Tart", "Red Velvet Cupcake"] #list
+business_hours = {"open": "11:00 AM", "close": "10:00 PM"} #dictionary
+menu_prices = {"Chocolate Cake": 150, "Blueberry Tart": 120, "Red Velvet Cupcake": 100, "Croissant": 80, "Cinnamon Roll": 90} #dictionary
+common_allergens = ["Gluten", "Dairy", "Eggs"] #list
+tax_rate = 0.08 #float
+
+#display(restaurant_name)
+#display(owner_name)
+#display(year_established)
+#display(popular_item_price)
+#display(has_delivery)
